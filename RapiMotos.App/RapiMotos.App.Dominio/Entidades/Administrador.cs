b95fde30f8b37cf95fc8fed2a7dc06ea6dc7e1d2
jk10s <juanka_267@hotmail.com>
2022-09-06 22:31:23 -0500

    update lawer

---
+++ RapiMotos.App/RapiMotos.App.Dominio/Entidades/Administrador.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RapiMotos.App.Dominio.Entidades
+{
+    public class Administrador:Persona
+    {
+        public string correo {get; set;}
+        
+    }
+}
---
+++ RapiMotos.App/RapiMotos.App.Dominio/Entidades/Persona.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RapiMotos.App.Dominio
+{
+    /// <summary>Class <c>Persona</c>
+    /// Modela una Persona en general en el sistema
+    /// </summary>
+    public class Persona
+    {
+        // Identificador único de cada persona
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string NumeroTelefono { get; set; }
+        
+        
+    }
+}
+
+
+
+namespace HospiEnCasa.App.Dominio
+{
+/// <summary>Class <c>Persona</c>
+/// Modela una Persona en general en el sistema
+/// </summary>
+public class Persona
+{
+    
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Apellidos { get; set; }
+    public string NumeroTelefono { get; set; }
+    // Genero de la persona
+    // public Genero Genero { set; get; }
+}
+}
