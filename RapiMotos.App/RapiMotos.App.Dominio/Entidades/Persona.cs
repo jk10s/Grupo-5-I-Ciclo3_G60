d93a15896ffb87370b14ee3e0705e3b90ebedb57
jk10s <juanka_267@hotmail.com>
2022-09-08 13:40:21 -0500

    update Persona

--- RapiMotos.App/RapiMotos.App.Dominio/Entidades/Persona.cs
+++ RapiMotos.App/RapiMotos.App.Dominio/Entidades/Persona.cs
@@ -22,19 +22,4 @@ namespace RapiMotos.App.Dominio
 
 
 
-namespace HospiEnCasa.App.Dominio
-{
-/// <summary>Class <c>Persona</c>
-/// Modela una Persona en general en el sistema
-/// </summary>
-public class Persona
-{
-    
-    public int Id { get; set; }
-    public string Nombre { get; set; }
-    public string Apellidos { get; set; }
-    public string NumeroTelefono { get; set; }
-    // Genero de la persona
-    // public Genero Genero { set; get; }
-}
-}
+
